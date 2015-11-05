335faca6ea6834189eeeaad010ba8233b4fdcadf
julianja12 <julian.ja@hotmail.com>
2015-11-05 16:36:07 -0500

    Prueba Reportes

---
+++ Clinica-ASP/ReporteHistoriaClinica.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting;
+namespace Clinica_ASP
+{
+    public partial class ReporteHistoriaClinica : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnReporte_Click(object sender, EventArgs e)
+        {
+            ReportViewer1.LocalReport.Refresh();
+        }
+    }
+}
